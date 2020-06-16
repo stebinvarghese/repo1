3e69d6e8656c6bc778e72456ed6af6be5f450562
Friends <Friends@Friends-PC.fios-router.home>
2020-06-15 20:01:27 -0400

    Commit and push by stebin

---
+++ Repo1/src/Secondpgm.java
@@ -0,0 +1,14 @@
+
+public class Secondpgm {
+
+	static int i;
+	static String name;	
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		System.out.println(i);
+		System.out.println(name);
+	}
+
+}
